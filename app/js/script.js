3fee2a17f5b367e0f000ab12b3a07bbcc5dd7386
trantoi99 <tranhungtoi308@gmail.com>
2020-10-06 16:26:13 +0700

    no message

--- app/js/script.js
+++ app/js/script.js
@@ -2,6 +2,45 @@ $(document).on('click', 'ul li', function() {
     $('li').removeClass('is-active');
     $(this).addClass('is-active');
 })
+$(document).ready(function() {
+    $('.slide').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        focusOnSelect: true,
+        prevArrow: '<a class="slide-arrow prev-arrow"><i class="lni lni-chevron-left" style="font-size:32px;"></i></a>',
+        nextArrow: '<a class="slide-arrow next-arrow"><i class="lni lni-chevron-right" style="font-size:32px;"></i></a>',
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        responsive: [{
+
+                breakpoint: 992,
+                settings: {
+                    arrows: false,
+
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+
+
+                    slidesToShow: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
 let countDown = new Date('December 31, 2020 00:00:00').getTime();
 let now = new Date().getTime();
 let distance = (countDown - now) / 1000;
@@ -21,4 +60,25 @@ let x = setInterval(function() {
     document.getElementById('hours').innerText = hours;
     document.getElementById('minutes').innerText = minutes;
     document.getElementById('seconds').innerText = seconds;
+
+
+    document.getElementById('day').innerText = days;
+    document.getElementById('hour').innerText = hours;
+    document.getElementById('minute').innerText = minutes;
+    document.getElementById('second').innerText = seconds;
 }, 1000);
+
+function increaseValue() {
+    var value = parseInt(document.getElementById('theInput').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('theInput').value = value;
+}
+
+function decreaseValue() {
+    var value = parseInt(document.getElementById('theInput').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value < 1 ? value = 1 : '';
+    value--;
+    document.getElementById('theInput').value = value;
+}
